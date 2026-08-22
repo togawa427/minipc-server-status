5cf025ec6b960113471dae040cf13e2b591c7d09
togawa427 <ssinya0922@gmail.com>
2026-08-23 00:43:40 +0900

    feat: process名を使った生存確認方法に変更

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ source .venv/bin/activate
 
 起動方法
 uvicorn main:app --reload
+
+サーバでの起動
+pm2を使用しています
--- routers/server.py
+++ routers/server.py
@@ -4,36 +4,43 @@ from cruds import server as server_cruds
 from schema import ItemCreate, ItemUpdate, ItemResponse
 import socket
 from pydantic import BaseModel;
+import subprocess
 
 class ServerStatus(BaseModel):
     status: str
     host: str
-    port: int
-
+    
 router = APIRouter(prefix="/servers", tags=["Servers"])
 
 @router.get("", response_model=list[ItemResponse], status_code=status.HTTP_200_OK)
 async def find_all():
     return server_cruds.find_all()
 
-# 同じホスト内のサーバが動いているかをポート確認する
+# 同じホスト内のサーバが動いているかをプロセス名から確認する
 @router.get("/{server_name}/status", status_code=status.HTTP_200_OK)
 async def get_status_by_name(server_name: str=Path()):
     host = "127.0.0.1"  # = localhost
     status = ""
     if(server_name == "valheim"):
-        # ポート検索
-        port = 2457
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(1)
-            result = s.connect_ex((host, port))
-        if(result == 0):
+        result = subprocess.run(
+            ["ps", "aux"],
+            capture_output=True,
+            text=True
+        )
+
+        # ps aux の結果から valheim_server を検索
+        is_running = any(
+            "valheim_server" in line.lower()
+            for line in result.stdout.splitlines()
+        )
+
+        if is_running:
             status = "running"
         else:
             status = "stopped"
+
         return ServerStatus(
             status=status,
-            host=host,
-            port=port
+            host="valheim"
         )
     raise HTTPException(status_code=404, detail="Server not found")
