ea7fba271320baa4209530b4c02a4b785c8a9624
togawa427 <ssinya0922@gmail.com>
2026-09-04 01:16:47 +0900

    feat: CORSを実装

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from cruds import server
 from routers import server
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*", "https://minipc-status.togetine.com"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # routersディレクトリのを読み込み
 app.include_router(server.router)
