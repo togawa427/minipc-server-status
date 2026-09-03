650cdb7bdaf2dda73e10042200b4add22465ce3e
togawa427 <ssinya0922@gmail.com>
2026-09-04 00:07:34 +0900

    feat: ミニPC自体のステータスも返すエンドポイント実装

--- routers/server.py
+++ routers/server.py
@@ -5,16 +5,72 @@ from schema import ItemCreate, ItemUpdate, ItemResponse
 import socket
 from pydantic import BaseModel;
 import subprocess
+import psutil
 
 class ServerStatus(BaseModel):
+    app_name: str
     status: str
-    host: str
-    
-router = APIRouter(prefix="/servers", tags=["Servers"])
 
-@router.get("", response_model=list[ItemResponse], status_code=status.HTTP_200_OK)
-async def find_all():
-    return server_cruds.find_all()
+class Status(BaseModel):
+    memory_total_mb: int
+    memory_usage_mb: int
+    cpu_usage_percent: int
+    app_servers: list[ServerStatus]
+
+    
+router = APIRouter(prefix="/api/v1/servers", tags=["Servers"])
+
+# @router.get("", response_model=list[ItemResponse], status_code=status.HTTP_200_OK)
+# async def find_all():
+#     return server_cruds.find_all()
+
+# 同じホスト内のサーバが動いているかをプロセス名から確認する
+@router.get("", status_code=status.HTTP_200_OK)
+async def get_status():
+
+    # ===== MiniPCのステータスを取得 =====
+    memory = psutil.virtual_memory()
+    memory_total_mb = memory.total // (1000 * 1000)
+    memory_usage_mb = memory.used // (1000 * 1000)
+
+    # ===== アプリサーバのステータスを取得 =====
+    app_statuses:list[ServerStatus] = []
+
+    status = ""
+    result = subprocess.run(
+        ["ps", "aux"],
+        capture_output=True,
+        text=True
+    )
+
+    # ps aux の結果から valheim_server を検索
+    is_running = any(
+        "valheim_server" in line.lower()
+        for line in result.stdout.splitlines()
+    )
+
+    if is_running:
+        status = "running"
+    else:
+        status = "stopped"
+
+    app_statuses.append(
+        ServerStatus(
+            app_name="valheim",
+            status=status,
+        )
+    )
+
+    # ===== レスポンスを返す =====
+    return Status(
+        memory_total_mb=memory_total_mb,
+        memory_usage_mb=memory_usage_mb,
+        cpu_usage_percent=int(psutil.cpu_percent(interval=1)),
+        app_servers= app_statuses
+    )
+
+    # raise HTTPException(status_code=404, detail="Server not found")
+
 
 # 同じホスト内のサーバが動いているかをプロセス名から確認する
 @router.get("/{server_name}/status", status_code=status.HTTP_200_OK)
@@ -41,6 +97,6 @@ async def get_status_by_name(server_name: str=Path()):
 
         return ServerStatus(
             status=status,
-            host="valheim"
+            app_name="valheim"
         )
     raise HTTPException(status_code=404, detail="Server not found")
